45f7546446c238fbb31ed6ac65b20712c7a85127
Zghiabui <buitrongnghiaaaaa@gmail.com>
2026-02-01 23:03:02 +0700

    demo part 3

---
+++ Assets/Script/EnemyHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        if (explosionPrefab != null)
+        {
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+}
